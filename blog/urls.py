2f83933053c5ac435f00ab34863bac9c0eaa64e7
oduodg <werewildfire@gmail.com>
2021-10-31 20:27:12 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [ # URL 패턴 추가
     path('',views.post_list, name='post_list'), 
     # post_list 라는 view가 루트 URL에 할당됨.
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
