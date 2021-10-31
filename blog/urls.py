d36e89ef6b199d391d48df7d918c9cec7d2fde12
oduodg <werewildfire@gmail.com>
2021-10-31 19:57:06 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [ # URL 패턴 추가
     path('',views.post_list, name='post_list'), 
     # post_list 라는 view가 루트 URL에 할당됨.
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
